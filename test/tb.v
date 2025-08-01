3897f2ea93881f04ed81704db6f7fee620f09a58
thexeno <thexeno@users.noreply.github.com>
2025-08-01 19:10:21 +0200

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -6,7 +6,12 @@
 */
 module tb ();
 
-
+  // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
+  initial begin
+    $dumpfile("tb.vcd");
+    $dumpvars(0, tb);
+    #1;
+  end
 
   // Wire up the inputs and outputs:
   reg clk;
@@ -17,14 +22,17 @@ module tb ();
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
-
+`ifdef GL_TEST
+  wire VPWR = 1'b1;
+  wire VGND = 1'b0;
+`endif
   // Replace tt_um_example with your module name:
   tt_um_thexeno_rgbw_controller user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
-      .VPWR(1'b1),
-      .VGND(1'b0),
+      .VPWR(VPWR),
+      .VGND(VGND),
 `endif
 
       .ui_in  (ui_in),    // Dedicated inputs
@@ -37,11 +45,5 @@ module tb ();
       .rst_n  (rst_n)     // not reset
   );
 
-    // Dump the signals to a VCD file. You can view it with gtkwave.
-  initial begin
-    $dumpfile("tb.vcd");
-    $dumpvars(0, tb);
-    
-  end
 
 endmodule
